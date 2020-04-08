39383f65d42eec2e4b21bc9e91b647e00cd210cd
nguyenmarvin8 <nguyenmarvin8@gmail.com>
2020-04-08 01:37:17 -0700

    changing response

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ app.get('/', (req, res) => {
   res.send(database.users);
 })
 
-app.get('/', (req, res) => { res.send(db.users) })
+app.get('/', (req, res) => { res.send('IT WORKS') })
 app.post('/signin', signin.handleSignin(db, bcrypt))
 app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
 app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
