817af5a100208b43193109b440f46ed33897e887
nguyenmarvin8 <nguyenmarvin8@gmail.com>
2020-04-08 02:44:55 -0700

    Update server.js

--- server.js
+++ server.js
@@ -13,8 +13,10 @@ const image = require('./controllers/image');
 const db = knex({
     client: 'pg',
     connection: {
-      connectionString : process.env.DATABASE_URL,
-      ssl: true,
+      host : '127.0.0.1',
+      user : '',
+      password : '',
+      database : 'face-recognition'
     } 
 });
 
@@ -27,7 +29,7 @@ app.use(bodyParser.json());
 //   res.send(database.users);
 // })
 
-app.get('/', (req, res) => { res.send('it is working!') })
+app.get('/', (req, res) => { res.send(db.users) })
 app.post('/signin', signin.handleSignin(db, bcrypt))
 app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
 app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
@@ -35,6 +37,6 @@ app.put('/image', (req, res) => { image.handleImage(req, res, db)})
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)})
 
 
-app.listen(process.env.PORT || 3000, ()=> {
-console.log(`app is running on port ${process.env.PORT}`);
-})
+app.listen(3000, ()=> {
+  console.log('app is running on port 3000');
+  })
