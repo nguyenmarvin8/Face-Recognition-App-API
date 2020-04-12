07cc75607f8956742570a95a28156242c30d18ef
nguyenmarvin8 <nguyenmarvin8@gmail.com>
2020-04-11 22:56:47 -0700

    fix

--- server.js
+++ server.js
@@ -13,10 +13,8 @@ const image = require('./controllers/image');
 const db = knex({
     client: 'pg',
     connection: {
-      host : '127.0.0.1',
-      user : '',
-      password : '',
-      database : 'face-recognition'
+      connectionString: process.env.DATABASE_URL,
+      ssl: true,
     } 
 });
 
@@ -25,18 +23,14 @@ const app = express();
 app.use(cors())
 app.use(bodyParser.json());
 
-// app.get('/', (req, res) => {
-//   res.send(database.users);
-// })
-
-app.get('/', (req, res) => { res.send(db.users) })
-app.post('/signin', signin.handleSignin(db, bcrypt))
+app.get('/', (req, res) => { res.send('Its working!') })
+app.post('/signin', (req, res) => {signin.handleSignin(req, res, db, bcrypt) })
 app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt) })
 app.get('/profile/:id', (req, res) => { profile.handleProfileGet(req, res, db)})
 app.put('/image', (req, res) => { image.handleImage(req, res, db)})
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res)})
 
 
-app.listen(3000, ()=> {
-  console.log('app is running on port 3000');
-  })
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`app is running on ${process.env.PORT}`);
+});
